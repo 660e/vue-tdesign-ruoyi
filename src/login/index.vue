b8a045ff3244ccc2d768ee3aaef64431e5148027
Min <solunar@vip.qq.com>
2025-03-13 21:33:39 +0800

    Update

---
+++ src/login/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import bgLogin from '@/assets/bg-login.png';
+
+const formData = reactive({
+  username: '',
+  password: '',
+});
+
+const onSubmit = () => {
+  console.log(formData);
+};
+</script>
+
+<template>
+  <div :style="{ backgroundImage: `url(${bgLogin})` }" class="h-screen bg-cover bg-right flex justify-center items-center">
+    <div class="p-6 rounded shadow bg-white">
+      <t-form :data="formData" @submit="onSubmit" class="w-80" label-width="0">
+        <t-form-item name="username">
+          <t-input v-model="formData.username" placeholder="账户" size="large" clearable>
+            <template #prefix-icon>
+              <t-icon name="desktop" />
+            </template>
+          </t-input>
+        </t-form-item>
+        <t-form-item name="password">
+          <t-input v-model="formData.password" placeholder="密码" size="large" type="password" clearable>
+            <template #prefix-icon>
+              <t-icon name="lock-on" />
+            </template>
+          </t-input>
+        </t-form-item>
+        <t-form-item>
+          <t-button size="large" theme="primary" type="submit" block>登录</t-button>
+        </t-form-item>
+      </t-form>
+    </div>
+  </div>
+</template>
--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -2,10 +2,15 @@ import type { RouteRecordRaw } from 'vue-router';
 
 export default {
   path: '/',
+  redirect: '/login',
   children: [
     {
-      path: '',
+      path: 'home',
       component: () => import('@/views/home/index.vue'),
+    },
+    {
+      path: 'login',
+      component: () => import('@/login/index.vue'),
     },
   ],
 } as RouteRecordRaw;
