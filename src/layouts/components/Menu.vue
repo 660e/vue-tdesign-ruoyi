55d9160748c6074e93f6c793ee4ce1be424a5322
Min <solunar@vip.qq.com>
2025-03-22 22:25:48 +0800

    Update

---
+++ src/layouts/components/Menu.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const collapsed = ref(false);
+</script>
+
+<template>
+  <div
+    @click="collapsed = !collapsed"
+    class="h-8 flex justify-center items-center cursor-pointer duration-200 border-b border-neutral-200 hover:bg-neutral-100"
+  >
+    <t-icon :name="`chevron-${collapsed ? 'right' : 'left'}-double`" size="20" />
+  </div>
+  <t-menu :collapsed="collapsed" class="flex-1 overflow-auto">
+    <t-menu-item value="1">
+      <template #icon>
+        <t-icon name="menu-application" />
+      </template>
+      <span>菜单1</span>
+    </t-menu-item>
+    <t-submenu title="菜单2" value="2">
+      <template #icon>
+        <t-icon name="menu-application" />
+      </template>
+      <t-menu-item value="2-1">
+        <span>菜单2-1</span>
+      </t-menu-item>
+      <t-menu-item value="2-2">
+        <span>菜单2-2</span>
+      </t-menu-item>
+    </t-submenu>
+  </t-menu>
+</template>
--- src/layouts/standard/index.vue
+++ src/layouts/standard/index.vue
@@ -2,13 +2,12 @@
 import Avatar from '../components/Avatar.vue';
 import Breadcrumb from '../components/Breadcrumb.vue';
 import Copyright from '../components/Copyright.vue';
+import Menu from '../components/Menu.vue';
 import More from '../components/More.vue';
 import Notification from '../components/Notification.vue';
 import Search from '../components/Search.vue';
 import Setting from '../components/Setting.vue';
 import Version from '../components/Version.vue';
-
-const collapsed = ref(false);
 </script>
 
 <template>
@@ -22,31 +21,7 @@ const collapsed = ref(false);
     </div>
     <div class="flex-1 overflow-auto flex">
       <div class="flex flex-col border-r border-neutral-200">
-        <div
-          @click="collapsed = !collapsed"
-          class="h-8 flex justify-center items-center cursor-pointer duration-200 border-b border-neutral-200 hover:bg-neutral-100"
-        >
-          <t-icon :name="`chevron-${collapsed ? 'right' : 'left'}-double`" size="20" />
-        </div>
-        <t-menu :collapsed="collapsed" class="flex-1 overflow-auto">
-          <t-menu-item value="1">
-            <template #icon>
-              <t-icon name="menu-application" />
-            </template>
-            <span>菜单1</span>
-          </t-menu-item>
-          <t-submenu title="菜单2" value="2">
-            <template #icon>
-              <t-icon name="menu-application" />
-            </template>
-            <t-menu-item value="2-1">
-              <span>菜单2-1</span>
-            </t-menu-item>
-            <t-menu-item value="2-2">
-              <span>菜单2-2</span>
-            </t-menu-item>
-          </t-submenu>
-        </t-menu>
+        <Menu />
         <Version class="h-8 text-xs flex justify-center items-center border-t border-neutral-200 text-neutral-400" />
       </div>
       <div class="flex-1 flex flex-col">
