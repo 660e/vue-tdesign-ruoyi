a2cba715e4f76c469ca37251d236b030f1dcb8b3
Min <solunar@vip.qq.com>
2025-03-22 11:29:49 +0800

    Update

--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -2,18 +2,8 @@ import type { RouteRecordRaw } from 'vue-router';
 
 export default {
   path: '/',
+  redirect: '/home',
   children: [
-    {
-      path: '',
-      component: () => import('@/layouts/standard/index.vue'),
-      children: [
-        {
-          path: '',
-          name: 'home',
-          component: () => import('@/views/home/index.vue'),
-        },
-      ],
-    },
     {
       path: 'login',
       name: 'login',
