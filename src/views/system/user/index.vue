97308201f3f73c1fa98e0c9e85c9541a4244d1e0
Min <solunar@vip.qq.com>
2025-03-24 22:11:53 +0800

    Update

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+import type { FormInstanceFunctions, FormProps } from 'tdesign-vue-next';
+
+const formRef = ref<FormInstanceFunctions>();
+const formData = reactive({
+  userName: '',
+});
+
+const onSubmit: FormProps['onSubmit'] = () => {
+  console.log(formData);
+};
+</script>
+
 <template>
-  <pre>{{ $route }}</pre>
+  <div class="h-full flex flex-col">
+    <div class="p-4">
+      <t-form :data="formData" @submit="onSubmit" class="flex gap-2" label-width="0" ref="formRef">
+        <t-form-item name="userName">
+          <t-input v-model="formData.userName" label="用户名称" />
+        </t-form-item>
+        <t-button type="reset" variant="outline">重置</t-button>
+        <t-button type="submit">搜索</t-button>
+      </t-form>
+    </div>
+    <div>tool</div>
+    <div class="flex-1">table</div>
+    <div>page</div>
+  </div>
 </template>
