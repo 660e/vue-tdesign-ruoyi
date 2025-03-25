9c53453d7485fece2b952917bb77f345de94621a
Min <solunar@vip.qq.com>
2025-03-25 22:35:32 +0800

    Update

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -18,7 +18,7 @@ const columns: TableProps['columns'] = [
     title: '用户名称',
   },
 ];
-const pagination: TableProps['pagination'] = reactive({
+const pagination = reactive({
   current: 1,
   pageSize: 10,
   total: 1000,
@@ -26,8 +26,36 @@ const pagination: TableProps['pagination'] = reactive({
 
 onMounted(() => {
   tableData.value = [
-    { id: 1, userName: '张三' },
-    { id: 2, userName: '李四' },
+    { id: 1, userName: '1' },
+    { id: 2, userName: '2' },
+    { id: 3, userName: '3' },
+    { id: 4, userName: '4' },
+    { id: 5, userName: '5' },
+    { id: 6, userName: '6' },
+    { id: 7, userName: '7' },
+    { id: 8, userName: '8' },
+    { id: 9, userName: '9' },
+    { id: 10, userName: '10' },
+    { id: 11, userName: '11' },
+    { id: 12, userName: '12' },
+    { id: 13, userName: '13' },
+    { id: 14, userName: '14' },
+    { id: 15, userName: '15' },
+    { id: 16, userName: '16' },
+    { id: 17, userName: '17' },
+    { id: 18, userName: '18' },
+    { id: 19, userName: '19' },
+    { id: 20, userName: '20' },
+    { id: 21, userName: '21' },
+    { id: 22, userName: '22' },
+    { id: 23, userName: '23' },
+    { id: 24, userName: '24' },
+    { id: 25, userName: '25' },
+    { id: 26, userName: '26' },
+    { id: 27, userName: '27' },
+    { id: 28, userName: '28' },
+    { id: 29, userName: '29' },
+    { id: 30, userName: '30' },
   ];
 });
 </script>
@@ -35,23 +63,83 @@ onMounted(() => {
 <template>
   <Page class="flex flex-col">
     <div class="px-4 pt-4">
-      <t-form :data="formData" @submit="onSubmit" class="flex gap-2" label-width="0" ref="formRef">
+      <t-form :data="formData" @submit="onSubmit" class="!space-x-2" label-width="0" layout="inline" ref="formRef">
         <t-form-item name="userName">
           <t-input v-model="formData.userName" label="用户名称" />
         </t-form-item>
-        <t-button theme="default" type="reset">重置</t-button>
-        <t-button type="submit">搜索</t-button>
+        <t-form-item name="userName">
+          <t-input v-model="formData.userName" label="用户名称" />
+        </t-form-item>
+        <t-form-item name="userName">
+          <t-input v-model="formData.userName" label="用户名称" />
+        </t-form-item>
+        <div class="flex-1"></div>
+        <t-button variant="text">
+          <template #icon>
+            <t-icon name="unfold-more" />
+          </template>
+        </t-button>
+        <t-button theme="default" type="reset">
+          <template #icon>
+            <t-icon name="refresh" />
+          </template>
+          <span>重置</span>
+        </t-button>
+        <t-button type="submit">
+          <template #icon>
+            <t-icon name="search" />
+          </template>
+          <span>搜索</span>
+        </t-button>
       </t-form>
     </div>
     <div class="p-4 flex gap-2 border-b border-neutral-200">
-      <t-button>新增</t-button>
-      <t-button theme="default">删除</t-button>
-      <t-button theme="default">导出</t-button>
-      <t-button theme="default">导入</t-button>
+      <t-button>
+        <template #icon>
+          <t-icon name="add" />
+        </template>
+        <span>新增</span>
+      </t-button>
+      <t-button theme="danger">
+        <template #icon>
+          <t-icon name="delete" />
+        </template>
+        <span>删除</span>
+      </t-button>
+      <div class="flex-1"></div>
+      <t-button shape="circle" variant="outline">
+        <template #icon>
+          <t-icon name="load" />
+        </template>
+      </t-button>
+      <t-button shape="circle" variant="outline">
+        <template #icon>
+          <t-icon name="view-column" />
+        </template>
+      </t-button>
+      <t-button shape="circle" variant="outline">
+        <template #icon>
+          <t-icon name="data-search" />
+        </template>
+      </t-button>
     </div>
-    <t-table :columns="columns" :data="tableData" class="flex-1" row-key="id" hover />
-    <div class="p-4">
-      <t-pagination :total="pagination.total" show-jumper />
+    <div class="flex-1 overflow-auto">
+      <t-table :columns="columns" :data="tableData" class="h-full" height="100%" row-key="id" hover />
+    </div>
+    <div class="p-4 flex gap-2">
+      <t-button theme="default">
+        <template #icon>
+          <t-icon name="upload" />
+        </template>
+        <span>导入</span>
+      </t-button>
+      <t-button theme="default">
+        <template #icon>
+          <t-icon name="download" />
+        </template>
+        <span>导出</span>
+      </t-button>
+      <t-pagination :total="pagination.total" class="flex-1" show-jumper />
     </div>
   </Page>
 </template>
