c16108bda935cd0e6ced6742844cb8c3dfa767a3
Min <solunar@vip.qq.com>
2025-04-19 22:00:53 +0800

    Update

--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -2,17 +2,12 @@ import type { RouteRecordRaw } from 'vue-router';
 
 export default {
   path: '/',
-  redirect: '/verify',
+  redirect: '/home',
   children: [
     {
       path: 'login',
       name: 'login',
       component: () => import('@/login/index.vue'),
-    },
-    {
-      path: 'verify',
-      name: 'verify',
-      component: () => import('@/layouts/components/Verify.vue'),
     },
   ],
 } as RouteRecordRaw;
