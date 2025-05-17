2589070552748ee8069b33ed5e39bbfe52b4b33e
Min <solunar@vip.qq.com>
2025-05-17 22:43:05 +0800

    Update

--- components.d.ts
+++ components.d.ts
@@ -22,8 +22,10 @@ declare module 'vue' {
     TLoading: typeof import('tdesign-vue-next')['Loading']
     TMenu: typeof import('tdesign-vue-next')['Menu']
     TMenuItem: typeof import('tdesign-vue-next')['MenuItem']
+    TPagination: typeof import('tdesign-vue-next')['Pagination']
     TProgress: typeof import('tdesign-vue-next')['Progress']
     TSubmenu: typeof import('tdesign-vue-next')['Submenu']
+    TTable: typeof import('tdesign-vue-next')['Table']
     TTooltip: typeof import('tdesign-vue-next')['Tooltip']
   }
 }
--- src/router/modules/dynamic.ts
+++ src/router/modules/dynamic.ts
@@ -1,10 +1,18 @@
 import type { IRoute } from '@/apis/types';
-// import router from '../index';
+import router from '../index';
+
+const views = import.meta.glob('@/views/**/index.vue', { eager: true });
 
 export async function initializeRouter(routes: IRoute[], layout: 'standard') {
-  console.log(layout);
+  console.log(views);
 
   routes.forEach((route) => {
     console.log(route);
+
+    router.addRoute(`layout-${layout}`, {
+      path: route.path,
+      redirect: '/',
+      name: route.name,
+    });
   });
 }
