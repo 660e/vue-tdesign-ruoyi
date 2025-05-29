9c6ddadbedfbc4cd42a881558097a7d330a8d106
Min <solunar@vip.qq.com>
2025-05-29 16:32:44 +0800

    Update

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,8 @@ import routes from './modules/static';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  scrollBehavior: () => ({ top: 0, left: 0 }),
   routes,
+  scrollBehavior: () => ({ top: 0, left: 0 }),
 });
 
 router.beforeEach(async (to, _, next) => {
