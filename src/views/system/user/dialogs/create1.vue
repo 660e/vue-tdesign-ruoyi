d9dc51e9f4b8d1f00c854f12634272ed52b9e28a
Min <solunar@vip.qq.com>
2025-05-29 19:06:57 +0800

    Update

---
+++ src/views/system/user/dialogs/create1.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import type { FormInstanceFunctions, FormProps, TableRowData } from 'tdesign-vue-next';
+import { getUser, deptTree, createUser, updateUser } from '@/apis/system';
+import { useLoading } from '@/hooks';
+import { useInfoStore } from '@/stores';
+
+const { showFullscreenLoading, hideFullscreenLoading } = useLoading();
+const { dicts } = useInfoStore();
+
+const visible = ref(false);
+const formRef = ref<FormInstanceFunctions>();
+const formData = reactive({
+  userId: undefined,
+  userName: '',
+  password: '',
+  nickName: '',
+  deptId: '',
+  phonenumber: '',
+  email: '',
+  sex: '2',
+  status: '0',
+  postIds: [],
+  roleIds: [],
+  remark: '',
+});
+const rules: FormProps['rules'] = {
+  userName: [{ required: true, trigger: 'blur' }],
+  password: [{ required: true, trigger: 'blur' }],
+  nickName: [{ required: true, trigger: 'blur' }],
+};
+const userData = ref();
+
+const show = async (row?: TableRowData) => {
+  showFullscreenLoading();
+  userData.value = await getUser(row?.userId);
+  if (row?.userId) {
+    Object.assign(formData, row);
+    formData.postIds = userData.value.postIds;
+    formData.roleIds = userData.value.roleIds;
+  }
+  userData.value.deptTree = (await deptTree()).data;
+
+  visible.value = true;
+  hideFullscreenLoading();
+};
+
+const onClosed = () => {
+  formRef.value?.reset();
+  formData.userId = undefined;
+};
+
+const onConfirm = async () => {
+  const result = await formRef.value?.validate();
+
+  if (result !== true) {
+    console.log(result); // TODO
+    return;
+  }
+
+  showFullscreenLoading();
+  try {
+    const { msg } = await (formData.userId ? updateUser(formData) : createUser(formData));
+    MessagePlugin.success(msg);
+    visible.value = false;
+  } catch {
+  } finally {
+    hideFullscreenLoading();
+  }
+};
+
+defineExpose({ show });
+</script>
+
+<template>
+  <t-dialog v-model:visible="visible" :header="`${formData.userId ? '修改' : '新增'}用户`" :on-closed="onClosed" :on-confirm="onConfirm" width="700">
+    <t-form :data="formData" :rules="rules" class="grid grid-cols-2" ref="formRef">
+      <t-form-item v-if="!formData.userId" label="用户名称" name="userName">
+        <t-input v-model="formData.userName" />
+      </t-form-item>
+      <t-form-item v-if="!formData.userId" label="密码" name="password">
+        <t-input v-model="formData.password" type="password" />
+      </t-form-item>
+      <t-form-item label="用户昵称" name="nickName">
+        <t-input v-model="formData.nickName" />
+      </t-form-item>
+      <t-form-item label="部门" name="deptId">
+        <t-tree-select v-model="formData.deptId" :data="userData?.deptTree" :keys="{ value: 'id' }" />
+      </t-form-item>
+      <t-form-item label="手机号码" name="phonenumber">
+        <t-input v-model="formData.phonenumber" />
+      </t-form-item>
+      <t-form-item label="邮箱" name="email">
+        <t-input v-model="formData.email" />
+      </t-form-item>
+      <t-form-item label="性别" name="sex">
+        <t-radio-group v-model="formData.sex">
+          <t-radio v-for="dict in dicts?.get('sys_user_sex')" :label="dict.label" :value="dict.value" :key="dict.value" />
+        </t-radio-group>
+      </t-form-item>
+      <t-form-item label="状态" name="status">
+        <t-radio-group v-model="formData.status">
+          <t-radio v-for="dict in dicts?.get('sys_normal_disable')" :label="dict.label" :value="dict.value" :key="dict.value" />
+        </t-radio-group>
+      </t-form-item>
+      <t-form-item class="col-span-2" label="岗位" name="postIds">
+        <t-select v-model="formData.postIds" :keys="{ label: 'postName', value: 'postId' }" :options="userData?.posts" multiple />
+      </t-form-item>
+      <t-form-item class="col-span-2" label="角色" name="roleIds">
+        <t-select v-model="formData.roleIds" :keys="{ label: 'roleName', value: 'roleId' }" :options="userData?.roles" multiple />
+      </t-form-item>
+      <t-form-item class="col-span-2" label="备注" name="remark">
+        <t-textarea v-model="formData.remark" />
+      </t-form-item>
+    </t-form>
+  </t-dialog>
+</template>
