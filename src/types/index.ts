4134f938a817107cc02317731b41974ad1a353a7
Min <solunar@vip.qq.com>
2025-06-01 09:38:40 +0800

    Update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
 import { DICTS } from '@/constants';
 
 export type AppSystemDictItem = (typeof DICTS)[number];
-export type TDictsMap = Map<AppSystemDictItem, { label: string; type: string; value: string }[]>;
+export type AppSystemDictMap = Map<AppSystemDictItem, { label: string; type: string; value: string }[]>;
