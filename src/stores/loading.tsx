ce8fa579b1803bbd7a67301345a68942515ad98b
Min <solunar@vip.qq.com>
2025-06-01 20:57:45 +0800

    Update

---
+++ src/stores/loading.tsx
@@ -0,0 +1,33 @@
+import type { LoadingInstance } from 'tdesign-vue-next';
+import type { AppFullscreenLoadingController } from '@/types';
+import { defineStore } from 'pinia';
+import { LoadingPlugin } from 'tdesign-vue-next';
+
+export const useFullscreenLoading = defineStore('loading', (): AppFullscreenLoadingController => {
+  const instance = ref<LoadingInstance>();
+  const count = ref(0);
+
+  const show = () => {
+    if (!instance.value) {
+      instance.value = LoadingPlugin({
+        size: '24px',
+        // indicator: () => <div class="animate-spin">Loading...</div>,
+      });
+    }
+    count.value += 1;
+    console.log('loading count:', count.value);
+  };
+
+  const hide = () => {
+    if (count.value > 0) {
+      count.value -= 1;
+    }
+    if (count.value === 0 && instance.value) {
+      instance.value.hide();
+      instance.value = undefined;
+    }
+    console.log('loading count:', count.value);
+  };
+
+  return { show, hide };
+});
