baa9500ea3844538f25ab8c513e424280d5d32e8
Min <solunar@vip.qq.com>
2025-06-08 17:17:49 +0800

    Update

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -9,6 +9,10 @@ export function login(data: { code: string; password: string; username: string; 
   return request.post('/login', { data });
 }
 
+export function logout() {
+  return request.post('/logout');
+}
+
 export function getInfo() {
   return request.get('/getInfo');
 }
