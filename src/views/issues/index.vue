0278ae248bed7c62867afa2ef3c6d7d947ce8bef
Min <solunar@vip.qq.com>
2025-06-09 18:40:03 +0800

    Update

--- src/views/issues/index.vue
+++ src/views/issues/index.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts">
-import { useFullscreenLoading } from '@/stores';
-
-const fullscreenLoading = useFullscreenLoading();
-
-const test = (timeout: number) => {
-  fullscreenLoading.show();
-  setTimeout(() => fullscreenLoading.hide(), timeout);
-};
-</script>
-
 <template>
-  <div class="h-screen flex justify-center items-center bg-black">
-    <div class="pt-40 flex gap-4 relative z-[9999]">
-      <t-button @click="test(1000)">1000</t-button>
-      <t-button @click="test(4000)">4000</t-button>
-      <t-button @click="test(7000)">7000</t-button>
-    </div>
-  </div>
+  <div></div>
 </template>
