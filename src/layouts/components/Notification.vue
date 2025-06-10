3c5ed1cbb199678a9debb4be73e0c7414ecb46c9
Min <solunar@vip.qq.com>
2025-06-10 09:33:53 +0800

    Update

--- src/layouts/components/Notification.vue
+++ src/layouts/components/Notification.vue
@@ -1,27 +1,29 @@
 <template>
-  <t-popup :overlay-inner-style="{ padding: 0 }" placement="top-right" trigger="click">
+  <t-popup placement="top-right" trigger="click">
     <div class="flex items-center gap-1 cursor-pointer duration-200 hover:text-blue-700">
       <div :style="{ backgroundColor: 'var(--td-error-color)' }" class="px-2 rounded-full text-xs text-white">100</div>
       <t-icon name="notification" />
     </div>
     <template #content>
       <div class="w-[600px]">
-        <div class="px-3 py-1.5">
-          <div>
-            <t-tag size="small" theme="primary" variant="light">通知</t-tag>
-            <span>Lorem ipsum dolor sit amet.</span>
-            <span class="flex-1"></span>
-            <span class="text-neutral-400">2025-06-09 18:00:00</span>
-          </div>
-          <div>
-            <t-tag size="small" theme="danger" variant="light">公告</t-tag>
-            <span>Natus tenetur veritatis aperiam cum?</span>
-            <span class="flex-1"></span>
-            <span class="text-neutral-400">2025-06-09 18:00:00</span>
-          </div>
+        <t-list size="small" split>
+          <t-list-item>
+            <t-tag class="!mr-2" size="small" theme="primary" variant="light">通知</t-tag>
+            <span>列表内容的描述性文字</span><span class="flex-1"></span><span class="text-neutral-400">2025-06-09 18:00:00</span>
+          </t-list-item>
+          <t-list-item>
+            <t-tag class="!mr-2" size="small" theme="primary" variant="light">通知</t-tag>
+            <span>列表内容的描述性文字</span><span class="flex-1"></span><span class="text-neutral-400">2025-06-09 18:00:00</span>
+          </t-list-item>
+          <t-list-item>
+            <t-tag class="!mr-2" size="small" theme="primary" variant="light">通知</t-tag>
+            <span>列表内容的描述性文字</span><span class="flex-1"></span><span class="text-neutral-400">2025-06-09 18:00:00</span>
+          </t-list-item>
+        </t-list>
+
+        <div class="pt-2 pb-1 flex justify-center">
+          <t-link theme="primary">查看更多...</t-link>
         </div>
-        <t-divider class="!my-0" />
-        <div>Lorem ipsum dolor sit amet.</div>
       </div>
     </template>
   </t-popup>
