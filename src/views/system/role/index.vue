e08ff4e4c83c11005d7c3175f81f87220685c9ff
Min <solunar@vip.qq.com>
2025-06-10 18:03:27 +0800

    Update

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -28,7 +28,19 @@ onMounted(async () => await onHandle('refresh'));
 
 <template>
   <Page class="flex">
-    <div class="w-96 border-r border-neutral-200"></div>
+    <div class="w-96 border-r border-neutral-200">
+      <t-list size="small" split>
+        <t-list-item v-for="row in tableData" class="duration-200 hover:bg-neutral-100" :key="row.roleId">
+          <div class="flex-1 flex items-center gap-2">
+            <span class="w-8 overflow-x-hidden font-mono text-right">{{ row.roleSort.toString().padStart(3, 0) }}</span>
+            <span>{{ row.roleName }}（{{ row.roleKey }}）</span>
+            <span class="flex-1"></span>
+            <q-table-tag-col :themes="['success', 'danger']" :value="row.status" dict="sys_normal_disable" />
+            <t-icon name="chevron-right" />
+          </div>
+        </t-list-item>
+      </t-list>
+    </div>
     <div class="flex-1">
       <pre>{{ tableData }}</pre>
     </div>
