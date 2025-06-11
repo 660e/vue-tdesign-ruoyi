162e4f8ac210312a4f2a1a14cd8b84d9b87db09a
Min <solunar@vip.qq.com>
2025-06-11 11:27:10 +0800

    Update

--- src/layouts/components/Version.vue
+++ src/layouts/components/Version.vue
@@ -1,7 +1,21 @@
+<!-- eslint-disable @typescript-eslint/no-explicit-any -->
 <script setup lang="ts">
 import PKG from '../../../package.json';
+
+const data = () => {
+  console.info({
+    language: navigator.language,
+    screen: {
+      devicePixelRatio: window.devicePixelRatio,
+      width: screen.width,
+      height: screen.height,
+    },
+    userAgentData: (navigator as any).userAgentData,
+    version: PKG.version,
+  });
+};
 </script>
 
 <template>
-  <div>{{ PKG.version }}</div>
+  <div @dblclick="data" class="cursor-default select-none">{{ PKG.version }}</div>
 </template>
