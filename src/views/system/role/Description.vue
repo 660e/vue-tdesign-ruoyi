923c0f578aa0029c6509bdacf834700d1f9f0584
Min <solunar@vip.qq.com>
2025-06-12 17:45:51 +0800

    Update

---
+++ src/views/system/role/Description.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex-1 flex flex-col">
+    <div class="p-4 flex">
+      <t-radio-group default-value="1" variant="default-filled">
+        <t-radio-button value="1">基本信息</t-radio-button>
+        <t-radio-button value="2">菜单权限</t-radio-button>
+        <t-radio-button value="3">数据权限</t-radio-button>
+        <t-radio-button value="4">分配用户</t-radio-button>
+      </t-radio-group>
+    </div>
+    <div class="flex-1 overflow-y-auto">
+      <div class="h-[2000px]"></div>
+    </div>
+  </div>
+</template>
--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -4,6 +4,7 @@ import { listRole } from '@/apis/system';
 import { Page } from '@/layouts/standard';
 import { useLoading } from '@/stores';
 import CreateDialog from './dialogs/Create.vue';
+import Description from './Description.vue';
 
 const createDialogRef = ref();
 const currentData = ref();
@@ -83,20 +84,7 @@ onMounted(async () => {
       </div>
     </div>
 
-    <div v-if="currentData" class="flex-1 flex flex-col">
-      <div class="p-4 flex">
-        <t-radio-group default-value="1" variant="default-filled">
-          <t-radio-button value="1">基本信息</t-radio-button>
-          <t-radio-button value="2">菜单权限</t-radio-button>
-          <t-radio-button value="3">数据权限</t-radio-button>
-          <t-radio-button value="4">分配用户</t-radio-button>
-        </t-radio-group>
-      </div>
-      <div class="flex-1 overflow-y-auto">
-        <div class="h-[2000px]"></div>
-      </div>
-    </div>
-
+    <Description v-if="currentData" :current-data="currentData" />
     <CreateDialog @confirm="onHandle('refresh')" ref="createDialogRef" />
   </Page>
 </template>
