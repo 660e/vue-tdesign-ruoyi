209458eeab2fa67fecff11b2f84c4353c520299c
Min <solunar@vip.qq.com>
2025-06-12 10:55:00 +0800

    Update

--- src/stores/loading.tsx
+++ src/stores/loading.tsx
@@ -1,6 +1,9 @@
 import type { LoadingInstance } from 'tdesign-vue-next';
 import { defineStore } from 'pinia';
 import { LoadingPlugin } from 'tdesign-vue-next';
+import NProgress from 'nprogress';
+
+NProgress.configure({ showSpinner: false });
 
 export const useFullscreenLoading = defineStore('loading', () => {
   const instance = ref<LoadingInstance>();
@@ -12,6 +15,7 @@ export const useFullscreenLoading = defineStore('loading', () => {
         size: '24px',
         // indicator: () => <div class="animate-spin">Loading...</div>,
       });
+      NProgress.start();
     }
     status.value += 1;
     console.info(`%c⏳ loading status: ${status.value}`, 'color: #0c0');
@@ -24,6 +28,7 @@ export const useFullscreenLoading = defineStore('loading', () => {
     if (status.value === 0 && instance.value) {
       instance.value.hide();
       instance.value = undefined;
+      NProgress.done();
     }
     console.info(`%c⏳ loading status: ${status.value}`, 'color: #0c0');
   };
