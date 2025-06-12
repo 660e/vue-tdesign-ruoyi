ec2f85a06b975f63eb6753fc5c517be4da371ae3
Min <solunar@vip.qq.com>
2025-06-12 22:06:20 +0800

    Update

--- src/views/system/role/Description.vue
+++ src/views/system/role/Description.vue
@@ -14,13 +14,32 @@ const onTabChange = (value: RadioValue) => {
 
 <template>
   <div class="flex-1 flex flex-col">
-    <div class="p-4 flex">
+    <div class="p-4 flex gap-2">
       <t-radio-group v-model="tab" :on-change="onTabChange" variant="default-filled">
         <t-radio-button :value="1">基本信息</t-radio-button>
         <t-radio-button :value="2">菜单权限</t-radio-button>
         <t-radio-button :value="3">数据权限</t-radio-button>
         <t-radio-button :value="4">分配用户</t-radio-button>
       </t-radio-group>
+
+      <template v-if="tab === 1">
+        <t-button @click="$emit('handle', 'edit', row)" theme="default">
+          <template #icon><t-icon name="edit" /></template><span>修改</span>
+        </t-button>
+        <t-button @click="$emit('handle', 'delete', row)" theme="danger">
+          <template #icon><t-icon name="delete" /></template><span>删除</span>
+        </t-button>
+      </template>
+      <template v-if="tab === 2">
+        <t-button>
+          <template #icon><t-icon name="save" /></template><span>保存</span>
+        </t-button>
+      </template>
+      <template v-if="tab === 3">
+        <t-button>
+          <template #icon><t-icon name="save" /></template><span>保存</span>
+        </t-button>
+      </template>
     </div>
 
     <div class="flex-1 overflow-y-auto">
@@ -33,14 +52,6 @@ const onTabChange = (value: RadioValue) => {
           <t-descriptions-item label="创建时间">{{ row.createTime }}</t-descriptions-item>
           <t-descriptions-item label="备注">{{ row.remark }}</t-descriptions-item>
         </t-descriptions>
-        <div class="pt-4 flex gap-2">
-          <t-button @click="$emit('handle', 'delete', row)" theme="danger">
-            <template #icon><t-icon name="delete" /></template><span>删除</span>
-          </t-button>
-          <t-button @click="$emit('handle', 'edit', row)" theme="default">
-            <template #icon><t-icon name="edit" /></template><span>修改</span>
-          </t-button>
-        </div>
       </div>
 
       <div v-if="tab === 2">菜单权限</div>
