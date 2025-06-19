eaeaae477b56d78f9501c82ba91feea25bdc097c
Min <solunar@vip.qq.com>
2025-06-19 12:28:16 +0800

    Update

--- src/views/system/dict/Description.vue
+++ src/views/system/dict/Description.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 import type { TableRowData } from 'tdesign-vue-next';
+import { listDictData } from '@/apis/system';
+import { useLoadingStore } from '@/stores';
 
 const { activeRowData } = defineProps<{ activeRowData: TableRowData }>();
+const loadingStore = useLoadingStore();
+const tableData = ref();
 
 const refresh = async () => {
-  console.log(activeRowData);
+  loadingStore.show();
+  try {
+    const { rows } = await listDictData({ pageNum: 1, pageSize: 9999, dictType: activeRowData.dictType });
+    tableData.value = rows;
+  } catch {
+  } finally {
+    loadingStore.hide();
+  }
 };
 
 watch(
@@ -17,6 +28,6 @@ onMounted(async () => await refresh());
 
 <template>
   <div class="flex-1 overflow-y-auto">
-    <pre>{{ activeRowData }}</pre>
+    <pre>{{ tableData }}</pre>
   </div>
 </template>
