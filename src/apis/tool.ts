11c9809efbee0581d8c792e80893c6535e75ef3f
Min <solunar@vip.qq.com>
2025-06-25 21:32:18 +0800

    Update

--- src/apis/tool.ts
+++ src/apis/tool.ts
@@ -3,6 +3,14 @@ import { request } from '@/utils';
 
 // ----- gen
 
+export function importTable(tables: string) {
+  return request.get<AppResponseData<AppUnknownRecord[]>>('/tool/gen/importTable', { params: { tables } });
+}
+
+export function listTable(params?: AppRequestListParams) {
+  return request.get<AppResponseData<AppUnknownRecord[]>>('/tool/gen/db/list', { params });
+}
+
 export function deleteGen(tableId: number | string) {
   return request.delete<AppResponseData<AppUnknownRecord>>(`/tool/gen/${tableId}`);
 }
--- src/views/tool/gen/dialogs/Create.vue
+++ src/views/tool/gen/dialogs/Create.vue
@@ -1,83 +1,28 @@
 <script setup lang="ts">
-import type { FormInstanceFunctions, FormProps, TableRowData } from 'tdesign-vue-next';
-import { createPost, updatePost } from '@/apis/system';
-import { useDict } from '@/hooks';
+import { listTable, importTable } from '@/apis/tool';
 import { useLoadingStore } from '@/stores';
 
 const { confirm } = defineProps<{ confirm: () => Promise<void> }>();
 const loadingStore = useLoadingStore();
 
 const visible = ref(false);
-const formRef = ref<FormInstanceFunctions>();
-const formData = reactive({
-  postId: undefined,
-  postSort: 0,
-  postName: '',
-  postCode: '',
-  status: '0',
-  remark: '',
-});
-const formRules: FormProps['rules'] = {
-  postSort: [{ required: true, trigger: 'blur' }],
-  postName: [{ required: true, trigger: 'blur' }],
-  postCode: [{ required: true, trigger: 'blur' }],
-};
 
-const show = (row?: TableRowData) => {
-  if (row?.postId) {
-    Object.assign(formData, row);
-  }
+const show = () => {
+  console.log(listTable);
   visible.value = true;
 };
 
-const onClosed = () => {
-  formRef.value?.reset();
-  formData.postId = undefined;
-};
-
 const onConfirm = async () => {
-  if ((await formRef.value?.validate()) !== true) return;
-
-  loadingStore.show();
-  try {
-    const { msg } = await (formData.postId ? updatePost : createPost)(formData);
-    await confirm();
-    MessagePlugin.success(msg);
-    visible.value = false;
-  } catch {
-  } finally {
-    loadingStore.hide();
-  }
+  console.log(importTable);
+  confirm();
+  loadingStore.hide();
 };
 
 defineExpose({ show });
 </script>
 
 <template>
-  <t-dialog
-    v-model:visible="visible"
-    :header="`${formData.postId ? '修改' : '新增'}岗位`"
-    :on-closed="onClosed"
-    :on-confirm="onConfirm"
-    placement="center"
-    width="500"
-  >
-    <t-form :data="formData" :rules="formRules" reset-type="initial" ref="formRef">
-      <t-form-item label="序号" name="postSort">
-        <t-input-number v-model="formData.postSort" />
-      </t-form-item>
-      <t-form-item label="岗位名称" name="postName">
-        <t-input v-model="formData.postName" />
-      </t-form-item>
-      <t-form-item label="岗位编码" name="postCode">
-        <t-input v-model="formData.postCode" />
-      </t-form-item>
-      <t-form-item label="状态" name="status">
-        <t-radio-group v-model="formData.status" :options="useDict('sys_normal_disable')" theme="button" variant="default-filled" />
-      </t-form-item>
-      <t-form-item label="备注" name="remark">
-        <t-textarea v-model="formData.remark" />
-      </t-form-item>
-    </t-form>
+  <t-dialog v-model:visible="visible" :on-confirm="onConfirm" header="新增" placement="center" width="500">
+    <div>Lorem ipsum dolor sit amet.</div>
   </t-dialog>
 </template>
