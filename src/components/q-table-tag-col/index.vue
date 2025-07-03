6f76b79689915c90a694488eaccfa9df4c4ec582
Min <solunar@vip.qq.com>
2025-07-03 12:09:01 +0800

    Update

--- src/components/q-table-tag-col/index.vue
+++ src/components/q-table-tag-col/index.vue
@@ -3,12 +3,12 @@ import type { TagProps } from 'tdesign-vue-next';
 import type { AppSystemDictKey } from '@/types';
 import { useDict } from '@/hooks';
 
-const { value, dict, themes } = defineProps<{ value: string; dict: AppSystemDictKey; themes: TagProps['theme'][] }>();
+const { value, dict, themes } = defineProps<{ value: number | string; dict: AppSystemDictKey; themes?: TagProps['theme'][] }>();
 
 const index = useDict(dict).findIndex((e) => e.value === String(value));
 const item = useDict(dict).find((e) => e.value === String(value));
 </script>
 
 <template>
-  <t-tag :theme="themes[index]" class="q-table-tag-col" size="small" variant="light-outline">{{ item?.label }}</t-tag>
+  <t-tag :theme="themes?.[index]" class="q-table-tag-col" size="small" variant="light-outline">{{ item?.label }}</t-tag>
 </template>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,11 @@
 export const DICTS = [
+  'sys_common_status',
   'sys_job_group',
   'sys_job_status',
   'sys_normal_disable',
   'sys_notice_status',
   'sys_notice_type',
+  'sys_oper_type',
   'sys_show_hide',
   'sys_user_sex',
   'sys_yes_no',
