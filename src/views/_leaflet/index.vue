89538d7d6e63d39745730273a6a0257c784b8a9e
Min <solunar@vip.qq.com>
2025-07-03 22:04:26 +0800

    Update

--- src/views/_leaflet/index.vue
+++ src/views/_leaflet/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import L from 'leaflet';
+
+onMounted(() => {
+  const map = L.map('map').setView([39.906217, 116.397428], 13);
+  L.tileLayer('https://webrd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}', {
+    minZoom: 6,
+    maxZoom: 18,
+  }).addTo(map);
+});
+</script>
+
 <template>
-  <pre>{{ $route }}</pre>
+  <div class="h-screen" id="map"></div>
 </template>
