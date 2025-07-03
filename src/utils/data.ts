659edda15393210e6964dda424532af911828420
Min <solunar@vip.qq.com>
2025-07-03 09:55:05 +0800

    Update

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -56,11 +56,11 @@ export function buildTree<T>(data: T[], options: { childrenKey?: string; idKey?:
   return result;
 }
 
-export function downloadBlob(blob: Blob, filename: string): void {
-  const url: string = URL.createObjectURL(blob);
-  const a: HTMLAnchorElement = document.createElement('a');
+export function downloadBlob(blob: Blob, fileName: string): void {
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
   a.href = url;
-  a.download = filename;
+  a.download = fileName;
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
