97c35a8018f51a73729866fc2dc3868fd5048f23
Min <solunar@vip.qq.com>
2025-07-03 18:15:52 +0800

    Update

--- src/views/monitor/cache/list.vue
+++ src/views/monitor/cache/list.vue
@@ -1,19 +1,61 @@
 <script setup lang="ts">
 import { listDictType, deleteDictType } from '@/apis/system';
-import { listCacheNames, clearCacheAll } from '@/apis/monitor';
+import { listCacheNames, listCacheKeys, getCacheValue, clearCacheAll } from '@/apis/monitor';
 import { useHandleDelete } from '@/hooks';
 import { Page } from '@/layouts/standard';
 import { useLoadingStore } from '@/stores';
-import CreateDialog from './dialogs/Create.vue';
-import Description from './Description.vue';
 
 const loadingStore = useLoadingStore();
 const namesData = ref();
-const activeNameData = ref();
+const activeName = ref();
+const keysData = ref();
+const activeKeyData = ref();
 
 const createDialogRef = ref();
 const tableData = ref();
 const activeRowData = ref();
+
+const clearAll = async () => {
+  loadingStore.show();
+  try {
+    const { msg } = await clearCacheAll();
+    MessagePlugin.success(msg);
+  } catch {
+  } finally {
+    loadingStore.hide();
+  }
+};
+
+const getNames = async () => {
+  loadingStore.show();
+  try {
+    namesData.value = (await listCacheNames()).data;
+  } catch {
+  } finally {
+    loadingStore.hide();
+  }
+};
+
+const getKeys = async (cacheName: string) => {
+  loadingStore.show();
+  try {
+    keysData.value = (await listCacheKeys(cacheName)).data;
+    activeName.value = cacheName;
+  } catch {
+  } finally {
+    loadingStore.hide();
+  }
+};
+
+const getValue = async (key: string) => {
+  loadingStore.show();
+  try {
+    activeKeyData.value = (await getCacheValue(activeName.value, key)).data;
+  } catch {
+  } finally {
+    loadingStore.hide();
+  }
+};
 
 const onHandle = async (value: string) => {
   switch (value) {
@@ -28,14 +70,6 @@ const onHandle = async (value: string) => {
       break;
 
     case 'clear-all':
-      loadingStore.show();
-      try {
-        const { msg } = await clearCacheAll();
-        MessagePlugin.success(msg);
-      } catch {
-      } finally {
-        loadingStore.hide();
-      }
       break;
 
     case 'refresh':
@@ -81,38 +115,41 @@ const onHandle = async (value: string) => {
   }
 };
 
-onMounted(async () => await onHandle('refresh-names'));
+onMounted(async () => await getNames());
 </script>
 
 <template>
   <Page class="flex">
-    <div class="w-1/4 min-w-80 flex flex-col border-r border-neutral-200">
+    <div class="w-80 flex flex-col border-r border-neutral-200">
       <div class="p-4 flex items-center gap-2 border-b border-neutral-200">
         <div class="w-1 h-full bg-neutral-200"></div>
         <span>缓存列表</span>
         <span class="flex-1"></span>
-        <t-button @click="onHandle('refresh-names')" theme="default">
+        <t-button @click="getNames" theme="default">
           <template #icon><t-icon name="refresh" /></template><span>刷新</span>
         </t-button>
-        <t-button @click="onHandle('clear-all')" theme="success">
+        <t-button @click="clearAll" theme="success">
           <template #icon><t-icon name="clear" /></template><span>清理</span>
         </t-button>
       </div>
       <div class="flex-1 overflow-y-auto pb-16">
         <t-list split>
           <t-list-item
-            v-for="name in namesData"
-            :style="{ backgroundColor: name.cacheName === activeNameData?.cacheName ? 'var(--td-brand-color-1)' : '' }"
-            @click="activeNameData = name"
+            v-for="row in namesData"
+            :style="{ backgroundColor: row.cacheName === activeName ? 'var(--td-brand-color-1)' : '' }"
+            @click="getKeys(row.cacheName)"
             class="cursor-pointer duration-200 hover:bg-neutral-100"
-            :key="name.cacheName"
+            :key="row.cacheName"
           >
             <div class="flex-1 flex items-center gap-2">
-              <span>{{ name.cacheName }}</span>
+              <span>{{ row.cacheName }}</span>
               <span class="flex-1"></span>
-              <t-icon name="chevron-right" />
+              <t-link class="flex items-center gap-0.5" theme="primary">
+                <t-icon name="clear" />
+                <span>清理</span>
+              </t-link>
               <b
-                :style="{ backgroundColor: 'var(--td-brand-color-7)', height: name.cacheName === activeNameData?.cacheName ? '100%' : '0' }"
+                :style="{ backgroundColor: 'var(--td-brand-color-7)', height: row.cacheName === activeName ? '100%' : '0' }"
                 class="absolute top-0 right-0 w-1 duration-200"
               ></b>
             </div>
@@ -121,21 +158,39 @@ onMounted(async () => await onHandle('refresh-names'));
       </div>
     </div>
 
-    <div v-if="activeRowData" class="flex-1 flex flex-col">
-      <div class="p-4 flex gap-2 border-b border-neutral-200">
-        <t-button @click="onHandle('copy')" theme="success">
-          <template #icon><t-icon name="copy" /></template><span>复制</span>
-        </t-button>
-        <t-button @click="onHandle('edit')" theme="default">
-          <template #icon><t-icon name="edit" /></template><span>修改</span>
-        </t-button>
-        <t-button @click="onHandle('delete')" theme="danger">
-          <template #icon><t-icon name="delete" /></template><span>删除</span>
+    <div v-if="keysData?.length" class="w-[400px] flex flex-col border-r border-neutral-200">
+      <div class="p-4 flex items-center gap-2 border-b border-neutral-200">
+        <div class="w-1 h-full bg-neutral-200"></div>
+        <span>键名列表</span>
+        <span class="flex-1"></span>
+        <t-button @click="onHandle('refresh-names')" theme="default">
+          <template #icon><t-icon name="refresh" /></template><span>刷新</span>
         </t-button>
       </div>
-      <Description :active-row-data="activeRowData" />
+      <div class="flex-1 overflow-y-auto pb-16">
+        <t-list split>
+          <t-list-item
+            v-for="row in keysData"
+            :style="{ backgroundColor: row === activeKeyData.cacheName ? 'var(--td-brand-color-1)' : '' }"
+            @click="getValue(row)"
+            class="cursor-pointer duration-200 hover:bg-neutral-100"
+            :key="row"
+          >
+            <div class="flex-1 flex items-center gap-2">
+              <span>{{ row }}</span>
+              <span class="flex-1"></span>
+              <t-link class="flex items-center gap-0.5" theme="primary">
+                <t-icon name="clear" />
+                <span>清理</span>
+              </t-link>
+              <b
+                :style="{ backgroundColor: 'var(--td-brand-color-7)', height: row === activeKeyData.cacheName ? '100%' : '0' }"
+                class="absolute top-0 right-0 w-1 duration-200"
+              ></b>
+            </div>
+          </t-list-item>
+        </t-list>
+      </div>
     </div>
-
-    <CreateDialog :on-success="async () => await onHandle('refresh')" ref="createDialogRef" />
   </Page>
 </template>
