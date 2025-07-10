4e29322f11147238619061f6cea67d3bb37f56ac
Min <solunar@vip.qq.com>
2025-07-10 12:45:15 +0800

    Update

--- src/components/l-map/index.vue
+++ src/components/l-map/index.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+import L from 'leaflet';
+
+const attrs = useAttrs() as {
+  id: string;
+};
+
+let map: L.Map | null = null;
+
+onMounted(() => {
+  if (!attrs.id) return;
+
+  map = L.map(attrs.id, {
+    center: [39.906217, 116.397428],
+    zoom: 13,
+    attributionControl: false,
+    zoomControl: false,
+  });
+  L.tileLayer('https://webrd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}', {
+    minZoom: 6,
+    maxZoom: 18,
+  }).addTo(map);
+});
+
+onUnmounted(() => {
+  if (!map) return;
+
+  map.remove();
+  map = null;
+});
+</script>
+
 <template>
-  <div>map</div>
+  <div>
+    <slot></slot>
+  </div>
 </template>
--- src/views/leaflet/index.vue
+++ src/views/leaflet/index.vue
@@ -1,27 +1,9 @@
 <script setup lang="ts">
 import { Page } from '@/layouts/standard';
-// import L from 'leaflet';
-
-// // https://webrd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}
-
-// let map: L.Map | null = null;
-
-// onMounted(() => {
-//   map = L.map('map', {
-//     center: [39.906217, 116.397428],
-//     zoom: 13,
-//     attributionControl: false,
-//     zoomControl: false,
-//   });
-//   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     minZoom: 6,
-//     maxZoom: 18,
-//   }).addTo(map);
-// });
 </script>
 
 <template>
   <Page>
-    <l-map class="h-full" />
+    <l-map class="h-full" id="map" />
   </Page>
 </template>
