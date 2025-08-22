b2d6549c5a4f985bf9822454b1a8b6f8703224c2
Min <solunar@vip.qq.com>
2025-08-22 18:38:21 +0800

    Update

--- src/views/leaflet/index.vue
+++ src/views/leaflet/index.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 // import { Page } from '@/layouts/standard';
 import { LMap, LMarker } from './components';
+
+const markers: [number, number][] = [
+  [39.91, 116.38],
+  [39.91, 116.43],
+];
 </script>
 
 <template>
   <!-- <Page> -->
-  <LMap :options="{ center: [39.906217, 116.397428] }" class="h-screen" id="map">
-    <LMarker :options="{ latlng: [39.906217, 116.397428] }" />
+  <LMap :options="{ center: [39.91, 116.4] }" class="h-screen" id="map">
+    <LMarker v-for="(marker, index) in markers" :options="{ latlng: marker }" :key="index" />
   </LMap>
   <!-- </Page> -->
 </template>
