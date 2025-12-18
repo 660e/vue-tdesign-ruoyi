9560c45d1b0353f8445ff0ab46ac37c00f121872
Min <solunar@vip.qq.com>
2025-12-18 22:04:07 +0800

    Update

--- src/layouts/admin/index.vue
+++ src/layouts/admin/index.vue
@@ -21,36 +21,39 @@ watchEffect(() => {
 
 <template>
   <div v-if="initialized" class="h-screen overflow-hidden flex flex-col">
-    <div class="h-14 px-4 flex items-center gap-4 border-b border-neutral-200">
-      <div class="flex items-center gap-2">
-        <img class="h-6" src="@/assets/logo.svg" />
-        <div class="text-xl">TDesign Starter</div>
-      </div>
-      <div class="flex-1"></div>
-      <Search />
-      <Avatar />
-    </div>
-    <div class="flex-1 overflow-auto flex">
-      <div class="flex flex-col border-r border-neutral-200">
-        <Menu />
-        <Version class="h-9 text-xs flex justify-center items-center border-t border-neutral-200 text-neutral-400" />
-      </div>
-      <div class="flex-1 flex flex-col overflow-x-auto">
-        <div class="h-9 px-4 flex items-center gap-2 border-b border-neutral-200">
-          <Breadcrumb />
-          <div class="flex-1"></div>
-          <Setting />
+    <RouterView v-if="fullscreen" />
+    <template v-else>
+      <div class="h-14 px-4 flex items-center gap-4 border-b border-neutral-200">
+        <div class="flex items-center gap-2">
+          <img class="h-6" src="@/assets/logo.svg" />
+          <div class="text-xl">TDesign Starter</div>
         </div>
-        <div class="flex-1 overflow-auto bg-neutral-100">
-          <RouterView />
+        <div class="flex-1"></div>
+        <Search />
+        <Avatar />
+      </div>
+      <div class="flex-1 overflow-auto flex">
+        <div class="flex flex-col border-r border-neutral-200">
+          <Menu />
+          <Version class="h-9 text-xs flex justify-center items-center border-t border-neutral-200 text-neutral-400" />
         </div>
-        <div class="h-9 px-4 flex items-center gap-2 border-t border-neutral-200">
-          <Copyright class="text-xs text-neutral-400" />
-          <div class="flex-1"></div>
-          <Notification />
+        <div class="flex-1 flex flex-col overflow-x-auto">
+          <div class="h-9 px-4 flex items-center gap-2 border-b border-neutral-200">
+            <Breadcrumb />
+            <div class="flex-1"></div>
+            <Setting />
+          </div>
+          <div class="flex-1 overflow-auto bg-neutral-100">
+            <RouterView />
+          </div>
+          <div class="h-9 px-4 flex items-center gap-2 border-t border-neutral-200">
+            <Copyright class="text-xs text-neutral-400" />
+            <div class="flex-1"></div>
+            <Notification />
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 
   <Initial v-else :done="() => (initialized = true)" />
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <pre>{{ $route }}</pre>
+  <div class="flex-1 overflow-y-auto">
+    <pre>{{ $route }}</pre>
+  </div>
 </template>
